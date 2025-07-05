0928721c6a5f7aead0fcb1f817f69aaa5dcf14e5
rhron255 <ronhasleton@gmail.com>
2025-07-05 19:18:18 +0300

    Initialized project from spring initializr.

---
+++ src/main/kotlin/rhron255/projects/k_dns/KDnsApplication.kt
@@ -0,0 +1,11 @@
+package rhron255.projects.k_dns
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KDnsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KDnsApplication>(*args)
+}
---
+++ src/test/kotlin/rhron255/projects/k_dns/KDnsApplicationTests.kt
@@ -0,0 +1,13 @@
+package rhron255.projects.k_dns
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KDnsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
