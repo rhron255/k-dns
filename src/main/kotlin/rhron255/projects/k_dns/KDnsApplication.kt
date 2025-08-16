89c710f96e9fc7b9e968981ab9d6b05e344f5aa7
rhron255 <ronhasleton@gmail.com>
2025-08-16 09:57:34 +0300

    # Implementation - Altered startup parameters.

--- src/main/kotlin/rhron255/projects/k_dns/KDnsApplication.kt
+++ src/main/kotlin/rhron255/projects/k_dns/KDnsApplication.kt
@@ -7,16 +7,16 @@ class KDnsApplication
 
 
 fun main(args: Array<String>) {
-    if (args.isEmpty()) {
-        KDnsServer().start()
-    } else if (args.size == 2) {
-        if (args[0] == "-") {
-            KDnsClient(args[1]).start()
+    if (args[0] == "server") {
+        KDnsServer(args[1]).start()
+    } else if (args[0] == "client") {
+        if (args[1] == "-") {
+            KDnsClient(args[2]).start()
         } else {
-            KDnsClient(args[1], args[0]).start()
+            KDnsClient(args[2], args[1]).start()
         }
     } else {
-        print("Usage: kdns <domain-name> <dns-address> OR kdns - <dns-address>")
+        print("Usage: kdns server <upstream-address> OR kdns client <domain-name> <dns-address> OR kdns client - <dns-address>")
         exitProcess(-1)
     }
 }
